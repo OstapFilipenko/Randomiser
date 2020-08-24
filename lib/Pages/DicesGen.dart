2e19d0360980f9daa2fcae9bff9d9130e11e0603
OstapFilipenko <info@ostapfilipenko.com>
2020-08-24 17:20:37 +0200

    added the dice page with its functionallity

--- lib/Pages/DicesGen.dart
+++ lib/Pages/DicesGen.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 class DicesGen extends StatefulWidget {
   @override
   _DicesGen createState() => _DicesGen();
 }
 
 class _DicesGen extends State<DicesGen> {
+  int numb = 1;
+  int oldnum;
+  var rng = new Random();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: new Center(
-        child: new Text("Dices"),
+        child: new FlatButton(
+          onPressed: () {
+            randomNumb(1, 7);
+          },
+          child: new Image(
+            image: AssetImage('assets/img/dice-$numb.png'),
+          ),
+        ),
       ),
     );
+  }
+
+  void randomNumb(var min, var max) {
+    oldnum = numb;
+    setState(() {
+      numb = min + rng.nextInt(max - min);
+      if (numb == oldnum) {
+        randomNumb(1, 7);
+      }
+    });
   }
 }
