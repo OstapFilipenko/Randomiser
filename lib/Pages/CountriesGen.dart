7c35044b3959cd5b518c2ae3dfa79bb024f4a3d0
OstapFilipenko <info@ostapfilipenko.com>
2020-08-26 21:35:35 +0200

    added wikipedia site to the generated country

--- lib/Pages/CountriesGen.dart
+++ lib/Pages/CountriesGen.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:iso_countries/iso_countries.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/foundation.dart';
 
 class CountriesGen extends StatefulWidget {
   @override
@@ -11,10 +15,12 @@ class CountriesGen extends StatefulWidget {
 
 class _CountriesGen extends State<CountriesGen> {
   List<Country> countryList;
+  WebViewController controller;
   Country country;
   int numb = 1;
   int oldnum;
   var rng = new Random();
+  var url = 'https://en.wikipedia.org/wiki/albania';
 
   void initState() {
     super.initState();
@@ -40,22 +46,45 @@ class _CountriesGen extends State<CountriesGen> {
     return Scaffold(
         body: new Center(
       child: countryList != null
-          ? new Card(
-              color: Colors.white,
-              child: new Container(
-                padding: EdgeInsets.all(20.0),
-                child: new Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    new Text(countryList[numb].name),
-                    new RaisedButton(
-                        child: new Text("generate"),
-                        onPressed: () {
-                          randomNumb(1, countryList.length);
-                        })
-                  ],
+          ? new ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                new Card(
+                  color: Colors.white,
+                  child: new Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: new Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new Text(countryList[numb].name),
+                        new RaisedButton(
+                            child: new Text("generate"),
+                            onPressed: () {
+                              randomNumb(1, countryList.length);
+                              setState(() {
+                                url = 'https://en.wikipedia.org/wiki/' +
+                                    countryList[numb].name;
+                                controller.loadUrl(url);
+                              });
+                            })
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+                new Container(
+                  height: MediaQuery.of(context).size.height,
+                  child: new WebView(
+                    initialUrl: url,
+                    javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (WebViewController webViewController) {
+                      controller = webViewController;
+                    },
+                    gestureRecognizers: Set()
+                      ..add(Factory<VerticalDragGestureRecognizer>(
+                          () => VerticalDragGestureRecognizer())),
+                  ),
+                )
+              ],
             )
           : new CircularProgressIndicator(),
     ));
